ed7024d6bd3d72f63dd9772bd4a03abb7b06a07b
Priscila <priscila.oliveiraa@gmail.com>
2020-02-07 21:05:47 -0500

    Added route for dropdown menu and css to index.html.

--- server/index.js
+++ server/index.js
@@ -4,10 +4,20 @@ const fetch = require('node-fetch');
 
 const app = express();
 
+const apis = {
+  player: 3001,
+  chats: 3002,
+  carousel: 3003,
+  channels: 3004,
 
+}
 app.use(express.static('public'));
 
-/* function apiHandler(path, req, res) {
+app.listen(3005, () => {
+  console.log('Listening on port 3005');
+  });
+
+function apiHandler(path, req, res) {
   if (path in apis) {
     return fetch(`http://${req.hostname}:${apis[path]}${req.url}`)
     .then((results) => results.json())
@@ -15,19 +25,23 @@ app.use(express.static('public'));
   };
 }
 
-const apis = {
-  chats: 3002,
-  carousel: 3003,
-  channels: 3004,
-}
 app.get('/api/chats/:videoId', (req, res) => {
   return apiHandler('chats', req, res);
 });
 
-app.get('/videos/userId', (req, res) => {
+app.get('/videos/:videoId', (req, res) => {
   return apiHandler('carousel', req, res);
 });
 
-app.listen(3005, () => {
-  console.log('Listening on port 3005');
-  }); */
+app.get('/api/channels/:videoId', (req, res) => {
+  return apiHandler('channels', req, res);
+});
+
+app.get('/api/livestream/:userId', (req, res) => {
+  return apiHandler('player', req, res);
+});
+
+app.get('/filter/:videoId/:categoryId', (req, res) => {
+  return apiHandler('carousel', req, res);
+});
+
